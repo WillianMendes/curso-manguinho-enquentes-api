766c8a1cd25b5a244e9ea06109b72a810ddcec12
Willian Mendes <willianmendesbr@icloud.com>
2022-03-22 20:36:57 -0300

    feat: sign-up controller returns 400 if no name is provided

---
+++ src/presentation/controller/signup.controller.ts
@@ -0,0 +1,10 @@
+class SignupController {
+  // eslint-disable-next-line no-unused-vars
+  public handle(httpRequest : any): any {
+    return {
+      statusCode: 400,
+    };
+  }
+}
+
+export default SignupController;
