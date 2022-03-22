2efb46b2f887ef2603b459327a371869a882a8a9
Willian Mendes <willianmendesbr@icloud.com>
2022-03-22 20:48:07 -0300

    feat: sign-up controller returns 400 if no email is provided

--- src/presentation/controller/signup.controller.ts
+++ src/presentation/controller/signup.controller.ts
@@ -1,10 +1,20 @@
 class SignupController {
-  // eslint-disable-next-line no-unused-vars
   public handle(httpRequest : any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name'),
-    };
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      };
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      };
+    }
+
+    return {};
   }
 }
 
