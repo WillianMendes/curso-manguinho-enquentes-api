bff78e651a01c8bb24c17dcbf8d91a799a8ffbf6
Willian Mendes <willianmendesbr@icloud.com>
2022-03-22 20:48:24 -0300

    test: sign-up controller returns 400 if no email is provided

--- src/presentation/controller/signup.controller.spec.ts
+++ src/presentation/controller/signup.controller.spec.ts
@@ -14,4 +14,18 @@ describe('SignupController Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   });
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignupController();
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        password: 'any_password',
+        password_confirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+  });
 });
