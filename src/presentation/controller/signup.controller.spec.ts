6a18f276163b7d0afdd017eab3257b9e8bf9a4d8
Willian Mendes <willianmendesbr@icloud.com>
2022-03-27 15:10:03 -0300

    refactor: add types to test

--- src/presentation/controller/signup.controller.spec.ts
+++ src/presentation/controller/signup.controller.spec.ts
@@ -1,31 +1,32 @@
 import SignupController from './signup.controller';
 import MissingParamException from '../exception/missing-param.exception';
+import { Request, Response } from '../protocol/http';
 
 describe('Signup Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignupController();
-    const httpRequest = {
+    const sut: SignupController = new SignupController();
+    const httpRequest: Request = {
       body: {
         email: 'any_email@email.com',
         password: 'any_password',
         password_confirmation: 'any_password',
       },
     };
-    const httpResponse = sut.handle(httpRequest);
+    const httpResponse: Response = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamException('name'));
   });
 
   test('Should return 400 if no email is provided', () => {
-    const sut = new SignupController();
-    const httpRequest = {
+    const sut: SignupController = new SignupController();
+    const httpRequest: Request = {
       body: {
         name: 'any name',
         password: 'any_password',
         password_confirmation: 'any_password',
       },
     };
-    const httpResponse = sut.handle(httpRequest);
+    const httpResponse: Response = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamException('email'));
   });
